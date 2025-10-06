9b58c1a3e2de0110e47e7457cba465c1f446a321
Jurij Jukić <j.jjukicc@gmail.com>
2025-10-06 14:53:05 +0200

    wip

--- src/hyperapp.rs
+++ src/hyperapp.rs
@@ -37,6 +37,7 @@ thread_local! {
 pub struct HttpRequestContext {
     pub request: IncomingHttpRequest,
     pub response_headers: HashMap<String, String>,
+    pub response_status: u16,
 }
 
 pub struct AppContext {
@@ -91,6 +92,15 @@ pub fn add_response_header(key: String, value: String) {
     APP_HELPERS.with(|helpers| {
         if let Some(ctx) = &mut helpers.borrow_mut().current_http_context {
             ctx.response_headers.insert(key, value);
+        }
+    })
+}
+
+// Set the HTTP response status code
+pub fn set_response_status(status: u16) {
+    APP_HELPERS.with(|helpers| {
+        if let Some(ctx) = &mut helpers.borrow_mut().current_http_context {
+            ctx.response_status = status;
         }
     })
 }
